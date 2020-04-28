c923790087a2d0b666ea039e52aeacaf866a08a5
Rose <1062572204@qq.com>
2020-04-28 23:51:14 +0800

    docker容器化部署实现

---
+++ mall-cache/src/main/java/com/wang/mall/cache/CacheApplication.java
@@ -0,0 +1,15 @@
+package com.wang.mall.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王念
+ * @create 2020-03-23 21:06
+ */
+@SpringBootApplication
+public class CacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class);
+    }
+}
---
+++ mall-security/src/main/java/com/wang/mall/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.wang.mall.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王念
+ * @create 2020-04-28 23:11
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
